3213cbb98225cafd99e3a714fa5e7d3bed2f8035
Wojciech Czerpak <wojciech.czerpak@gmail.com>
2016-01-02 23:55:10 +0100

    update dependencies in main project. Change Joke Java lib logic. Place hardcoded jokes.

--- Build_It_Bigger/chuck-norris-jokes/src/main/java/com/example/Joke.java
+++ Build_It_Bigger/chuck-norris-jokes/src/main/java/com/example/Joke.java
@@ -4,11 +4,23 @@ package com.example;
  * Created by fares on 02.01.16.
  */
 public class Joke {
-    private final int mId;
-    private final String mJoke;
 
-    public Joke(int mId, String mJoke) {
-        this.mId = mId;
-        this.mJoke = mJoke;
+    private final String[] mJokes = new String[] {
+            "Chuck Norris uses ribbed condoms inside out, so he gets the pleasure.",
+            "There are no steroids in baseball. Just players Chuck Norris has breathed on.",
+            "Chuck Norris doesn't read books. He stares them down until he gets the information he wants.",
+            "In an average living room there are 1,242 objects Chuck Norris could use to kill you, including the room itself.",
+            "CNN was originally created as the \"Chuck Norris Network\" to update Americans with on-the-spot ass kicking in real-time."
+    };
+
+    private int mCounter = 0;
+
+    public String[] getAll(){
+        return mJokes;
     }
+
+    public String getNext(){
+        return mJokes[mCounter++%mJokes.length];
+    }
+
 }
