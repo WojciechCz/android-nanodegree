151d881e1d44529130fa27434cb71c48d51f13d4
Wojciech Czerpak <wojciech.czerpak@gmail.com>
2016-01-03 14:13:41 +0100

    backend works on google cloud

--- Build_It_Bigger/app/src/main/java/com/udacity/gradle/builditbigger/EndpointTask.java
+++ Build_It_Bigger/app/src/main/java/com/udacity/gradle/builditbigger/EndpointTask.java
@@ -9,8 +9,6 @@ import com.example.Joke;
 import com.example.fares.builditbigger.backend.myApi.MyApi;
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
-import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
-import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 
 import java.io.IOException;
 
@@ -24,18 +22,9 @@ public class EndpointTask extends AsyncTask<Pair<Context, String>, Void, Joke> {
     @Override
     protected Joke doInBackground(Pair<Context, String>... params) {
         if(myApiService == null) {  // Only do this once
-            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
-                    new AndroidJsonFactory(), null)
-                    // options for running against local devappserver
-                    // - 10.0.2.2 is localhost's IP address in Android emulator
-                    // - turn off compression when running against local devappserver
-                    .setRootUrl("http://192.168.1.15:8080/_ah/api/")
-                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
-                        @Override
-                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
-                            abstractGoogleClientRequest.setDisableGZipContent(true);
-                        }
-                    });
+            MyApi.Builder builder =
+                    new MyApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
+                    .setRootUrl("https://build-it-bigger-endpoint.appspot.com/_ah/api/");
 
             myApiService = builder.build();
         }
